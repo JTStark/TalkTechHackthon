06f813131e0de90cfeca5b5992a1026c908ce673
diegokamiha <diego.kamiha@clubedaentrega.com.br>
2016-03-19 18:29:53 -0300

    Events names fixed

--- irbot/irbot.py
+++ irbot/irbot.py
@@ -16,11 +16,11 @@ class IngressoRapidoBot(object):
     commands = {
         'event': event_handler,
         'start': start_handler,
-	   'eventos-no-dia': events_on_handler,
-        'eventos-em': events_at_handler,
-        'eventos-do-tipo': events_type_handler,
+	   'eventosnodia': events_on_handler,
+        'eventosem': events_at_handler,
+        'eventosdotipo': events_type_handler,
         'busca': search_handler,
-	   'eventos-no-periodo': events_between_handler
+	   'eventosnoperiodo': events_between_handler
 
     }
 
