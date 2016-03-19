835f4cab5cdd5e08bf74d0d429e45407bb0604a3
Thiago Pinto <thspinto@gmail.com>
2016-03-19 14:10:45 -0300

    Resultados da busca mokeados.

---
+++ irbot/busca_ingresso_rapido.py
@@ -0,0 +1,21 @@
+SITE_URL = 'https://www.ingressorapido.com.br/'
+
+mock_event = {
+    'event_url': SITE_URL + 'Evento.aspx?ID=46865',
+    'image_url': SITE_URL + '/ImagensSistema/Eventos/eI047372thumb.jpg',
+    'title': 'Titulo',
+    'local': 'Lugar',
+    'pages': 1
+}
+
+def search_event_by_name(event, page_number=1):
+    return [mock_event]
+
+def search_event_by_city(city, page_number=1):
+    return [mock_event]
+
+def search_event_by_category(category, page_number=1):
+    return [mock_event]
+
+def search_event_by_date(start_date, end_date, page_number=1):
+    return [mock_event]
