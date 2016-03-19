9123e7d40e3f70b821ddbbdc779344c940e464f0
Lucas Tadeu Teixeira <ltadeut@outlook.com>
2016-03-19 13:47:44 -0300

    Adiciona o script para iniciar o bot

---
+++ start.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from irbot.irbot import IngressoRapidoBot
+
+
+if __name__ == '__main__':
+    token = os.environ.get('IRBOT_TOKEN', '')
+
+    # Se o token não foi configurado como variável de ambiente,
+    # encerramos o programa sem lançar o bot
+    if not token:
+        sys.stderr.write('Erro: variável de ambiente IRBOT_TOKEN não está configurada.')
+
+    IngressoRapidoBot(token).run()
